02009485c0a18c2dce01488bea6dd7d7ed183564
ReeperC3 <cian2neiland@gmail.com>
2025-12-21 12:55:26 +0000

    File loads correctly and chapters display when file loads. Course is saved to localStorage

--- index.js
+++ index.js
@@ -3,19 +3,61 @@
 const jsonInput = document.getElementById("json-input")
 const fileLoadBtn = document.getElementById("file-load-btn")
 const errorMsg = document.getElementById("error-msg")
+const courseDiv = document.getElementById("course")
 
 fileLoadBtn.addEventListener("click", () => {
-    if (!jsonInput.files[0]) {
+    const file = jsonInput.files[0]
+
+    if (!file) {
         errorMsg.innerHTML = "No file attached"
         return
     }
 
-    let data = jsonInput.files[0]
-    if (data) {
-        errorMsg.innerHTML = ""
+    const reader = new FileReader()
+
+    reader.onload = () => {
+        try {
+            const json = JSON.parse(reader.result)
+            localStorage.setItem("course", JSON.stringify(json))
+            errorMsg.innerHTML = ""
+            loadCourse()
+        } catch {
+            errorMsg.innerHTML = "Invalid JSON file"
+        }
     }
-    else {
-        errorMsg.innerHTML = "Incorrect file format"
-        return 
-    }
+
+    reader.readAsText(file)
 })
+
+function loadCourse() {
+    const raw = localStorage.getItem("course")
+    if (!raw) return   
+
+    courseDiv.children[0].innerHTML = ""
+    courseDiv.children[1].innerHTML = ""
+
+    const data = JSON.parse(raw)
+    courseDiv.children[0].innerHTML = data.name
+    for (let i = 1; i < Object.keys(data["chapters"]).length + 1; i++) {
+        let chapterDiv = document.createElement("div")
+        let chapterName = document.createElement("h4")
+        let chapterList = document.createElement("ul")
+
+        let currentChapter = data["chapters"][`Chapter ${i}`]
+        let lessonsNum = currentChapter["lessons"].length
+
+        chapterName.innerHTML = `${currentChapter["name"]}`
+
+        for (let j = 0; j < lessonsNum; j++) {
+            lessonLink = document.createElement("li")
+            lessonLink.innerHTML = currentChapter["lessons"][j]
+            chapterList.appendChild(lessonLink)
+        }
+
+        chapterDiv.appendChild(chapterName)
+        chapterDiv.appendChild(chapterList)
+        courseDiv.children[1].appendChild(chapterDiv)
+    }
+}
+
+loadCourse()
